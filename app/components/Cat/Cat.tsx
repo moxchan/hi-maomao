e07e788ad323de1c6b3c117125c8f70628719569
Max Chan <maxwellczy@gmail.com>
2025-12-15 21:06:16 +1100

    feat(app): basically fully functional

--- app/components/Cat/Cat.tsx
+++ app/components/Cat/Cat.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import { useState } from "react";
-
 import Image from "next/image";
-
-type CatState = "happy" | "sad" | "complete";
+import { useAppStore } from "@/app/stores/appStore";
 
 export const Cat = () => {
-  const [catState, setCatState] = useState<CatState>("happy");
+  const { fails, win } = useAppStore();
 
-  switch (catState) {
-    case "complete":
-      return;
-    case "sad":
-      return;
-    case "happy":
-    default:
-      return <Image src="/happy.gif" alt={""} width={360} height={360} />;
+  if (win) {
+    return (
+      <Image src="/standingcatflowers.png" alt={""} width={864} height={882} />
+    );
   }
+
+  if (fails > 2) {
+    return <Image src="/sadwhitecat.png" alt={""} width={732} height={554} />;
+  }
+
+  return <Image src="/happy.gif" alt={""} width={360} height={360} />;
 };
--- app/components/MainScreen/MainScreen.tsx
+++ app/components/MainScreen/MainScreen.tsx
@@ -1,19 +1,39 @@
+import { useAppStore } from "@/app/stores/appStore";
 import { Button } from "..";
 import { Cat } from "..";
 
 import styles from "./MainScreen.module.css";
 
 export const MainScreen = () => {
+  const { win } = useAppStore();
+
   return (
     <div className={styles.root}>
       <Cat />
-      <p
-        className={styles.question}
-      >{`Would you like to go on a date the night before NYE? <3`}</p>
-      <div className={styles.buttonCluster}>
-        <Button type="yes" />
-        <Button type="no" />
-      </div>
+      {!win && (
+        <p
+          className={styles.question}
+        >{`Would you like to go on a date the night before NYE? <3`}</p>
+      )}
+      {!win && (
+        <div className={styles.buttonCluster}>
+          <Button type="yes" />
+          <Button type="no" />
+        </div>
+      )}
+
+      {win && <p className={styles.yay}>{`Yay!`}</p>}
+      {win && (
+        <p className={styles.details}>{`
+      I'll take you to a spot on the mountain, let's pretend it's NYE (:
+      `}</p>
+      )}
+
+      {win && (
+        <p className={styles.details}>{`
+      Text me for more deets bc i can't fit much on here ):
+      `}</p>
+      )}
     </div>
   );
 };
